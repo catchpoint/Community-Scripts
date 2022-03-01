85ba331f9f69030760dad2003aad89574f1a9354
Amit Vikram <avikram@catchpoint.com>
2022-03-02 00:55:57 +0530

    Python 2.7 Code restructured

---
+++ Node Status Monitor/Python v2.7/node_monitor.py
@@ -0,0 +1,16 @@
+# from configparser import ConfigParser
+import os
+import logging
+import sys
+current_working_directory = os.getcwd()
+sys.path.insert(0, current_working_directory+'/libs')
+sys.path.insert(0, current_working_directory+'/utils')
+from common import check_node_field_changes
+from logger import info_logger,error_logger
+
+
+## Main function
+try:
+    check_node_field_changes()
+except Exception as e:
+    error_logger.exception(str(e))
