0e5235b485d5e8ed40ebbf01b815979bbf82f6f6
Amit Vikram <avikram@catchpoint.com>
2022-03-02 00:55:57 +0530

    Python 2.7 Code restructured

---
+++ Node Status Monitor/Python v2.7/utils/application_constants.py
@@ -0,0 +1,12 @@
+# Constants
+nodes_url='https://io.catchpoint.com/ui/api/v1/nodes?targeted=true'
+token_url='https://io.catchpoint.com/ui/api/token'
+current_session_data_file="new_node_data.json"
+previous_session_data_file="old_node_data.json"
+output_folder_path='Output'
+raw_data_folder_path='details'
+default_lookup_fields=['status.name']
+logger_info_folder_path='logs/info'
+logger_error_folder_path='logs/error'
+logger_info_file_Name='/info.log'
+logger_error_file_Name='/error.log'
